11f2b99d3c5e3e7b7f4321589ebc0418f8619244
kazu12222 <90078114+kazu12222@users.noreply.github.com>
2022-02-18 20:30:15 +0900

    Update quiz_tweet.js

--- quiz_tweet.js
+++ quiz_tweet.js
@@ -1,4 +1,4 @@
-let siteInfomation = require("./get_quiz.js");
+let quizhubHandler = require("./get_quiz.js");
 require("dotenv").config();
 const { TwitterApi } = require("twitter-api-v2");
 const { WaitTask } = require("puppeteer");
@@ -15,7 +15,7 @@ const twitterClient = new TwitterApi({
   const rwClient = twitterClient.readWrite;
   const appOnlyClientFromConsumer = await twitterClient.appLogin();
   const quizSiteUrl = process.env.QUIZ_SITE_URL;
-  const [quizzes, answerPublishDate] = await siteInfomation.getQuizElement(
+  const [quizzes, answerPublishDate] = await quizhubHandler.getQuizElement(
     quizSiteUrl
   );
   for (let i = 0; i < quizzes.length; i++) {
