318cbdb219c1c82317311d540c8780d8f5f59f23
kazu12222 <90078114+kazu12222@users.noreply.github.com>
2022-02-21 14:17:17 +0900

    Update quiz_tweet.js

--- quiz_tweet.js
+++ quiz_tweet.js
@@ -1,72 +1,49 @@
-<template>
-  <b-container>
-    <div class="row">
-      <div class="col-sm-12 col-lg-8 mx-auto mt-4 mb-4">
-        <b-card border-variant="dark" class="mt-3" header="ログイン">
-          <div v-if="isLoading" class="loading">
-            <b-spinner class="loading-spinner" variant="primary" label="Spinning"/>{{loadingMessage}}
-	  </div>
-          <div v-show="!isLoading" id="firebaseui-auth-container"/>
-        </b-card>
-      </div>
-    </div>
-  </b-container>
-</template>
+let quizhubHandler = require("./get_quiz.js");
+require("dotenv").config();
+const { TwitterApi } = require("twitter-api-v2");
+const { WaitTask } = require("puppeteer");
 
-<script>
-import firebase from '~/plugins/firebase'
-const firebaseui = require('firebaseui-ja')
-import 'firebaseui-ja/dist/firebaseui.css'
-
-import { mapActions, mapMutations, mapState, mapGetters } from 'vuex'
-
-  export default {
-    name: "login",
-    data(){
-      return {
-        isLoading: false,
-	loadingMessage: '読み込み中...'
-      }
-    },
-    mounted(){
-      this.isLoading = true
-      const redirectUrl = this.$route.query.redirect_url || '/'
-      const ui = firebaseui.auth.AuthUI.getInstance() || new firebaseui.auth.AuthUI(firebase.auth());
-
-      ui.start('#firebaseui-auth-container', {
-        signInSuccessUrl: redirectUrl,
-        signInFlow: 'popup',
-        signInOptions: [
-          firebase.auth.EmailAuthProvider.PROVIDER_ID,
-          firebase.auth.TwitterAuthProvider.PROVIDER_ID,
-        ],
-        callbacks: {
-          signInSuccessWithAuthResult: (authResult, redirectUrl) => {
-            this.loadingMessage = 'ログイン処理中...'	  
-            this.isLoading = true
-            return true
-        },
-          uiShown: () => {
-            this.isLoading = false
-          }
-        }
-      })
-    },
-    methods: {
-      logout(){
-        firebase.auth().signOut()
-      },
-      ...mapMutations('user', ['setUserInfo'])
+// Instanciate with desired auth type (here's Bearer v2 auth)
+const twitterClient = new TwitterApi({
+  appKey: process.env.CONSUMER_KEY,
+  appSecret: process.env.CONSUMER_SECRET,
+  accessToken: process.env.ACCESS_TOKEN_KEY,
+  accessSecret: process.env.ACCESS_TOKEN_SECRET,
+});
+// Play with the built in methods
+(async () => {
+  const rwClient = twitterClient.readWrite;
+  const appOnlyClientFromConsumer = await twitterClient.appLogin();
+  const quizSiteUrl = process.env.QUIZ_SITE_URL;
+  const [quizzes, answerPublishDate] = await quizhubHandler.getQuizElement(
+    quizSiteUrl
+  );
+  for (let i = 0; i < quizzes.length; i++) {
+    const href = quizzes[i].href;
+    const title = quizzes[i].title;
+    const user = quizzes[i].user;
+    let createTime = quizzes[i].createTime;
+    const stringTime = quizzes[i].createTime.toString();
+    const getStringTime = stringTime.slice(0, 10);
+    createTime = isNaN(getStringTime)
+      ? getStringTime.replace(/[^0-9/]|/g, "")
+      : null;
+    if (answerPublishDate[i] !== null) {
+      let ansOpenDate = answerPublishDate[i];
+      const stringAnsDay = ansOpenDate.toString();
+      ansOpenDate = isNaN(stringAnsDay)
+        ? stringAnsDay.replace(/[^0-9/]|/g, "")
+        : null;
     }
-  }
-</script>
+    const selectTweet = answerPublishDate[i]
+      ? `日は${ansOpenDate}`
+      : "は公開済み";
+    const tweetContents = `${user}さんから新規Quizご登録いただきました！\n一般解答公開${selectTweet}です。\n\n${title}\n #QuizHub\n${href}`;
+    await twitterClient.v2.tweet(tweetContents);
 
-<style scoped>
-.loading{
-  display: flex;
-  justify-content: center
-}
-.loading-spinner{
-  margin: 0 0.5em
-}
-</style>
+    async function sleep() {
+      await new Promise((s) => setTimeout(s, 3000));
+    }
+    sleep();
+  }
+})();
